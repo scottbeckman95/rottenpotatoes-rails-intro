9bd97ac5f19649939a6ebfb84451ad2039e45eeb
Scott Beckman <sbeckma2@uccs.edu>
2018-02-21 01:29:55 +0000

    Committing homework 2.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,51 +7,29 @@ class MoviesController < ApplicationController
   end
 
   def index
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering,@title_header = {:order => :title}, 'hilite'
+    when 'release_date'
+      ordering,@date_header = {:order => :release_date}, 'hilite'
+    end
     @all_ratings = Movie.all_ratings
-    redirect = false
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
 
-    logger.debug(session.inspect)
-
-    if params[:sort_by]
-      @sort_by = params[:sort_by]
-      session[:sort_by] = params[:sort_by]
-    elsif session[:sort_by]
-      @sort_by = session[:sort_by]
-      redirect = true
-    else
-      @sort_by = nil
-    end
-
-    if params[:commit] == "Refresh" and params[:ratings].nil?
-      @ratings = nil
-      session[:ratings] = nil
-    elsif params[:ratings]
-      @ratings = params[:ratings]
-      session[:ratings] = params[:ratings]
-    elsif session[:ratings]
-      @ratings = session[:ratings]
-      redirect = true
-    else
-      @ratings = nil
-    end
-
-    if redirect
+    if params[:sort] != session[:sort]
+      session[:sort] = sort
       flash.keep
-      redirect_to movies_path :sort_by=>@sort_by, :ratings=>@ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
 
-    if @ratings and @sort_by
-      @movies = Movie.where(:rating => @ratings.keys).find(:all, :order => (@sort_by))
-    elsif @ratings
-      @movies = Movie.where(:rating => @ratings.keys)
-    elsif @sort_by
-      @movies = Movie.find(:all, :order => (@sort_by))
-    else
-      @movies = Movie.all
+    if params[:ratings] != session[:ratings] and @selected_ratings != {}
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      flash.keep
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
-    if !@ratings
-      @ratings = Hash.new
-    end
+    @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
   end
 
   def new
@@ -80,6 +58,17 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+
+  def same_director
+    id = params[:id]
+    @movie = Movie.find(id)
+    begin
+      @movies = Movie.find_same_director(id)
+    rescue Movie::NoDirectorInfo => exception
+      flash[:warning] = "'#{@movie.title}' has no director info."
+      redirect_to movies_path
+    end
   end
 
 end
